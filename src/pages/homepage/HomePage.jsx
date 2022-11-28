b462d82a2785f900ddc2482a7c7cf676cc56242c
DevRaphael7 <raphaelramalho1989@gmail.com>
2022-11-28 11:53:11 -0300

    Maker filter frontend

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -2,7 +2,51 @@ import './HomePage.css'
 
 export const HomePage = () => {
 
-    return <section>
+    const produtos = [
+        {
+            "nome": "Celular",
+            "preco": 450.0,
+            "cover": "https://i.zst.com.br/thumbs/12/28/3a/-550637268.jpg"
+        },
+        {
+            "nome": "Relógio digital",
+            "preco": 34.0,
+            "cover": "https://static.dafiti.com.br/p/Touro-Boots-Rel%C3%B3gio-Digital-Touro-Boots-Pulseira-Silicone-Preto-2432-4714674-1-zoom.jpg"
+        },
+        {
+            "nome": "Xbox One",
+            "preco": 2500.5,
+            "cover": "https://files.tecnoblog.net/wp-content/uploads/2020/12/Xbox-One-500GBa.jpg"
+        },
+        {
+            "nome": "1984 - George Orwell",
+            "preco": 15.99,
+            "cover": "https://m.media-amazon.com/images/I/819js3EQwbL.jpg"
+        }
+    ]
+
+    const getProdutos = () => {
+        return produtos.map(value => (
+            <div class="product-container">
+                <img src={value.cover} />
+                <div className='info'>
+                    <h4>{ value.nome }</h4>
+                    <br />
+                    <p>{ value.preco.toFixed(2) } R$</p>
+                    <br />
+                    <button>
+                        Ver mais
+                    </button>
+                </div>
+            </div>
+        ))
+    }
+
+    const filtrar = (e) => {
+        console.log(e.target.value)
+    }
+
+    return <section style={{'background': '#f2f2f2'}} className="h-full">
         
         <header>
             <div class="cabecalho">
@@ -41,8 +85,23 @@ export const HomePage = () => {
             </nav>
         </header>
 
+        <section className='products'>
+            <div className='flex justify-spacer filtro'>
+                <p>Filtro</p>
 
+                <select name="select" onChange={filtrar}>
+                    <option value="1">Ordem crescente</option>
+                    <option value="2">Ordem descrecente</option>
+                    <option value="3">Ordem alfabética</option>
+                </select>
+            </div>
 
+            <hr />
+
+            <div className="products-grid">
+                { getProdutos() }
+            </div>
+        </section>
 
     </section>
 }
